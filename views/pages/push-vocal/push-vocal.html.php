749b1982d61852a5a6e5d802aece2511e1a0d126
Mouhamet99 <dieyemouhamet1@gmail.com>
2023-02-20 12:02:59 +0000

    do the same for push-vocal page

--- views/pages/push-vocal/push-vocal.html.php
+++ views/pages/push-vocal/push-vocal.html.php
@@ -3,37 +3,36 @@
 <meta name="description" content="Campagne audio sur les téléphones. Activez vos pubs et messages vers vos cibles en toute langue. ">
 <meta name="keywords" content="campagnes par vocal, voix, voice api, campagnes push vocal">
 
-<?php 
-    include("push-vocal.css.php");
-    echo(START_TAGS);
-    require_once(ROOT_VIEWS_INC."menu.inc.php");
+<?php
+include("push-vocal.css.php");
+echo (START_TAGS);
+require_once(ROOT_VIEWS_INC . "menu.inc.php");
 ?>
 
 <!-- hero section  -->
 <section class="hero-section hero-section-sm">
-    <div class="container">
+    <div class="container mt-4">
         <div class="text-center">
             <div class="col">
                 <h2>Push <span>Vocal</span></h2>
-                <p>
-                    Envoyez et diffusez un message vocale dans la langue de votre choix 
-                    a vos clients en faisant sonner leurs téléphones.
-                </p>
             </div>
         </div>
     </div>
 </section>
 
 <!-- second section  -->
-<section class="second-section py-2">
+<section class="second-section py-5">
     <div class="container">
+        <p class="text-center mb-5 pb-5">
+            Envoyez et diffusez un message vocale <br> dans la langue de votre choix
+            à vos clients en faisant sonner leurs téléphones. </p>
         <div class="row">
             <div class="col text-center order-2">
                 <div class="d-flex flex-column justify-content-center h-100">
                     <h3>Faites sonner les téléphones</h3>
                     <p class="mt-2">
-                        Un moyen pratique d’émettre des messages vocaux à travers les téléphones mobiles, 
-                        mais également fixes. Il s’utilise notamment dans les campagnes de marketing. Ce service 
+                        Un moyen pratique d’émettre des messages vocaux à travers les téléphones mobiles,
+                        mais également fixes. Il s’utilise notamment dans les campagnes de marketing. Ce service
                         permet d’informer, d’alerter, et même de questionner.
                     </p>
                     <div class="mt-2">
@@ -45,7 +44,7 @@
             </div>
             <div class="col order-1 ">
                 <div class="h-100 d-flex align-items-center justify-content-center">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/ring.webp')?>" alt="ring">
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/slider7.png') ?>" alt="ring">
                 </div>
             </div>
         </div>
@@ -61,8 +60,7 @@
             bonne nouvelle, c’est que vous pouvez y accéder.
         </p>
         <div class="btn pt-3">
-            <a target="_blank"
-            href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
+            <a target="_blank" href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
         </div>
     </div>
 </section>
@@ -73,7 +71,7 @@
         <div class="row">
             <div class="col col-12 col-md-4">
                 <div class="d-flex align-items-center justify-content-center">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/push.webp')?>" alt="adafri">
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/push.webp') ?>" alt="adafri">
                 </div>
             </div>
             <div class="col col-12 col-md-8">
@@ -89,13 +87,13 @@
     </div>
 </section>
 
-<!-- join section  --> 
+<!-- join section  -->
 <section class="join-section py-5">
     <div class="container">
         <div class="row join-cards">
             <div class="col col-12 col-md-6">
                 <div class="card">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/A.svg')?>" alt="adafri">        
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/A.svg') ?>" alt="adafri">
                     <h6>Commencez l'aventure ADAFRI</h6>
                     <p class="my-2">Profitez des services de notre outil marketing et faites grandir<br> votre business.</p>
                     <div class="text-start">
@@ -105,17 +103,14 @@
             </div>
             <div class="col col-12 col-md-6 mt-5 mt-sm-0">
                 <div class="card card-right">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/white-headset.png')?>" alt="adafri">        
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/white-headset.png') ?>" alt="adafri">
                     <h6>Commencez l'aventure ADAFRI</h6>
                     <p class="my-2">Profitez des services de notre outil marketing et faites grandir<br> votre business.</p>
                     <div class="text-start">
-                        <a target="_blank"
-                        href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
+                        <a target="_blank" href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-
