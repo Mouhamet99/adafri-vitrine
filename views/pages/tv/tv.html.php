14025941387e6b6c74d037cbf23259a4617e3d1c
Mouhamet99 <dieyemouhamet1@gmail.com>
2023-02-20 12:26:33 +0000

    do the same for tv page

--- views/pages/tv/tv.html.php
+++ views/pages/tv/tv.html.php
@@ -3,29 +3,29 @@
 <meta name="description" content="Diffusez vos produits et services sur les chaines de télévisions en Afrique.">
 <meta name="keywords" content="plateforme de pubs, faire passer ma pub a la tele, publicités tele au Sénégal">
 
-<?php 
-    include("tv.css.php");
-    echo(START_TAGS);
-    require_once(ROOT_VIEWS_INC."menu.inc.php");
+<?php
+include("tv.css.php");
+echo (START_TAGS);
+require_once(ROOT_VIEWS_INC . "menu.inc.php");
 ?>
 
 <!-- hero section  -->
 <section class="hero-section hero-section-sm">
-    <div class="container">
+    <div class="container mt-4">
         <div class="text-center">
             <div class="col">
                 <h2><span>Ecritels</span> sur les <span>chaines télés locales</span></h2>
-                <p>
-                    Passez directement au <span>message</span> et <span>faites retenir votre numéro</span>
-                </p>
             </div>
         </div>
     </div>
 </section>
 
 <!-- second section  -->
-<section class="second-section py-2">
+<section class="second-section py-5">
     <div class="container">
+        <p class="text-center mb-5 pb-5">
+            Passez directement au message et faites retenir votre numéro
+        </p>
         <div class="row">
             <div class="col text-start order-2">
                 <div class="d-flex flex-column justify-content-center h-100">
@@ -43,7 +43,7 @@
             </div>
             <div class="col order-1 ">
                 <div class="h-100 d-flex align-items-center justify-content-center">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/tv.webp')?>" alt="ussd survey">
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/tv.webp') ?>" alt="ussd survey">
                 </div>
             </div>
         </div>
@@ -59,9 +59,7 @@
             bonne nouvelle, c’est que vous pouvez y accéder.
         </p>
         <div class="btn pt-3">
-            <a 
-            target="_blank"
-            href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">
+            <a target="_blank" href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">
                 Prendre rendez-vous
             </a>
         </div>
@@ -74,7 +72,7 @@
         <div class="row join-cards">
             <div class="col col-12 col-md-6">
                 <div class="card">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/A.svg')?>" alt="adafri">        
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/A.svg') ?>" alt="adafri">
                     <h6>Commencez l'aventure ADAFRI</h6>
                     <p class="my-2">Profitez des services de notre outil marketing et faites grandir<br> votre business.</p>
                     <div class="text-start">
@@ -84,12 +82,11 @@
             </div>
             <div class="col col-12 col-md-6 mt-5 mt-sm-0">
                 <div class="card card-right">
-                    <img src="<?php echo(WEB_ROOT.'assets/images/white-headset.png')?>" alt="adafri">        
+                    <img src="<?php echo (WEB_ROOT . 'assets/images/white-headset.png') ?>" alt="adafri">
                     <h6>Commencez l'aventure ADAFRI</h6>
                     <p class="my-2">Profitez des services de notre outil marketing et faites grandir<br> votre business.</p>
                     <div class="text-start">
-                        <a target="_blank"
-                        href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
+                        <a target="_blank" href="https://meetings-eu1.hubspot.com/dieuveille-goma/notre-1er-rendez-vous-?__hstc=97983424.89c537ad01fe42754e5b0e07974778c5.1669894721085.1675682490897.1675688328305.26&__hssc=97983424.1.1675688328305&__hsfp=3058040093" class="green-btn">Prendre rendez-vous</a>
                     </div>
                 </div>
             </div>
