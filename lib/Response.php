35f52b264d3a3a256d27402257bc1b813a46378e
Mouhamet99 <dieyemouhamet1@gmail.com>
2023-02-22 16:43:33 +0000

    use is_callable instead method_exists to ensure that method can be called without causing any errors

--- lib/Response.php
+++ lib/Response.php
@@ -4,12 +4,7 @@ namespace ism\lib;
 class Response {
 
     public static function redirectUrl(string $uri):void{
-        // var_dump("uri", $uri);
-        // die();
-        // if($uri == "accueil")
-        //     header("location:".WEB_ROOT);
-        // else
-            header("location:".WEB_ROOT.$uri);
+        header("location:".WEB_ROOT.$uri);
         exit();
     }
 }
--- lib/Router.php
+++ lib/Router.php
@@ -17,14 +17,16 @@ class Router  {
         $array_uri = $this->request->getUri();
         $array_uri = str_replace('-', '_', $array_uri);
       
-        if(empty($array_uri[0]) )Response::redirectUrl("accueil");
-        $action=$array_uri[0];
-        if(strpos($action,"."))Response::redirectUrl("accueil");
+        // if(empty($array_uri[0]) )Response::redirectUrl("");
+        $action = empty($array_uri[0]) ? "accueil" : $array_uri[0];
+        if(strpos($action,"."))Response::redirectUrl("");
         $controller=ucfirst("PageController");
+        
         if(file_exists(ROOT.DIRECTORY_SEPARATOR."controllers".DIRECTORY_SEPARATOR.$controller.".php")){
             $controllerClass= "ism\\controllers\\".$controller;
             $objectController = new $controllerClass();
-            if(method_exists($objectController,$action)){
+            
+            if(is_callable(array($objectController, $action))){
                 call_user_func([$objectController,$action],$this->request);
             }else{
                 $this->ctlrError->pageNotFound();
